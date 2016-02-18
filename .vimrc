16f873c8bbd562c2f5ce790b7660164e93d1415e
Joon Guillen <joon@modulogeek.com>
2016-02-18 14:15:25 -0600

    Now with vim-pathogen and vim-salt

--- .vimrc
+++ .vimrc
@@ -5,3 +5,7 @@ set background=dark
 nmap <F9> :set number! number?<cr>
 nmap <F10> :set paste! paste?<cr>
 nmap <F11> :set ignorecase! ignorecase?<cr>
+
+execute pathogen#infect()
+filetype plugin indent on
+set nocompatible
--- install.sh
+++ install.sh
@@ -4,10 +4,13 @@
 
 THISDIR="`dirname $0`"
 
+echo "Copying ~/.vim"
+cp -a $THISDIR/.vim ~/.vim
 echo "Copying ~/.vimrc"
 cp $THISDIR/.vimrc ~/.vimrc
 echo "Copying ~/.screenrc"
 cp $THISDIR/.screenrc ~/.screenrc
 echo "Explicit permissions.."
-chmod 640 .vimrc .screenrc
+chmod 640 ~/.vimrc ~/.screenrc
+chmod o-rwx ~/.vim
 echo "Done."
