15150950b90855a732d81d9e3638e1ed43e4e1b6
Joon Guillen <joon@modulogeek.com>
2016-04-07 11:39:29 -0500

    Now with minimal mode!

--- install.sh
+++ install.sh
@@ -2,21 +2,33 @@
 # Install Joon's default
 # Linux environment!
 
+INSTALLTYPE="$1"
 THISDIR="`dirname $0`"
 vundle_dir="$HOME/.vim/bundle/Vundle.vim"
 vundle_git="https://github.com/VundleVim/Vundle.vim.git"
 
-echo "Copying ~/.vim"
-cp -a $THISDIR/.vim ~/.vim
-echo "Copying ~/.vimrc"
-cp $THISDIR/.vimrc ~/.vimrc
+
 echo "Copying ~/.screenrc"
 cp $THISDIR/.screenrc ~/.screenrc
+
+echo "Copying ~/.vim"
+cp -a $THISDIR/.vim ~/.vim
+
+echo "Copying ~/.vimrc"
+if [ "$INSTALLTYPE" = 'minimal' ]; then
+  echo "(minimal install mode)"
+  cp $THISDIR/.vimrc_minimal ~/.vimrc
+else
+  cp $THISDIR/.vimrc ~/.vimrc
+  if [ ! -d "$vundle_dir" ]; then
+    echo "Installing Vundle.vim..."
+    git clone $vundle_git $vundle_dir
+  fi
+  echo "Done."
+fi
+
 echo "Explicit permissions.."
 chmod 640 ~/.vimrc ~/.screenrc
 chmod o-rwx ~/.vim
-if [ ! -d "$vundle_dir" ]; then
-  echo "Installing Vundle.vim..."
-  git clone $vundle_git $vundle_dir
-fi
-echo "Done."
+
+echo "Done!"
